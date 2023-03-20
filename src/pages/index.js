f55dc3498f2c6227e535238e3abc99bbdc539a09
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-03-20 14:03:00 +0100

    use layout component in home page index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
-const IndexPage = () => {
-  return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
-}
+const IndexPage = () => (
+
+  
+    // <main>
+    //   <h1>Welcome to my Gatsby site!</h1>
+    //   <Link to='/about'>about</Link>
+    //   <p>I'm making this by following the Gatsby Tutorial.</p>
+    // </main>
+
+    <Layout pageTitle="home page haha">
+          <p>I'm making this by following the Gatsby Tutorial. this text is children</p>
+    </Layout>
+    
+
+
+)
 
 // You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
