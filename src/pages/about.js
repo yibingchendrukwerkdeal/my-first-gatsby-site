a9ebc2442851c98fad5933e08bc97df8ea5c5d0a
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-03-20 14:01:28 +0100

    added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,21 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
-const AboutPage = () => {
-  return (
+const AboutPage = () => (
     <main>
       <h1>About Me</h1>
+      <Link to='/'>home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-
+      <h1>Favorite food </h1>
+      <p>sushi sushi sushi sushi </p>
+      <h1>next vacation destination</h1>
+      <p>poland</p>
     </main>
-  )
-}
+)
 
-
-//export const Head=()=><title>about me</title>
-
+export const Head=()=><title>About me</title>
 
 
 // Step 3: Export your component
