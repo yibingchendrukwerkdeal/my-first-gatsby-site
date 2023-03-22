15d2a2e54689b2789ddcc91e6bcfa4d1a476b41a
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-03-22 15:32:45 +0100

    finished part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
     
   ],
 }
--- src/pages/blog/{MDX.frontmatter__slug}.js
+++ src/pages/blog/{MDX.frontmatter__slug}.js
@@ -2,12 +2,18 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost=({data,children})=>{
-    
+    const image=getImage(data.mdx.frontmatter.hero_image)
+
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>posted on: {data.mdx.frontmatter.date}</p>
+            <GatsbyImage 
+              image={image} 
+              alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <br/>
             {children}
         </Layout>
@@ -16,14 +22,22 @@ const BlogPost=({data,children})=>{
 
 export const Head=()=><Seo title="Blog posts"></Seo>
 
-export const query=graphql`query ($id:String) {
-    mdx (id:{eq:$id}){
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+export const query=graphql`query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
       }
     }
-  }`
+  }
+}`
 
 
 export default BlogPost
